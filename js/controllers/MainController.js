ce6aa8074a567c0dfd7ff46e184f09209e5a9bb8
codebyanya <sembyanya@gmail.com>
2018-06-24 14:00:33 +0200

    minor changes in main controller

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,7 +1,7 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.program =
     { series: 'Sherlock',
-     series_img: "./img/sherlock.svg",
+     series_img: "img/sherlock.svg",
      genre: 'Crime drama',
      season: 3,
      episode: "The Empty Hearse",
